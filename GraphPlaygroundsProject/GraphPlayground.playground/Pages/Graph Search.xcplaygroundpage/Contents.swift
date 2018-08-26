a490f759041df9d4a0478859a0eafae81450fc7b
Karsten Gresch <git-2015@by-doing.org>
2018-08-26 23:25:17 +0200

    Graph Playground Content

--- GraphPlaygroundsProject/GraphPlayground.playground/Pages/Graph Search.xcplaygroundpage/Contents.swift
+++ GraphPlaygroundsProject/GraphPlayground.playground/Pages/Graph Search.xcplaygroundpage/Contents.swift
@@ -8,28 +8,32 @@ let graph = Graph()
 let unitA = Entity(type: "Unit")
 unitA["name"] = "UnitA"
 unitA["isExpensive"] = true
-
-let unitC = Entity(type: "Unit")
-unitC["name"] = "UnitC"
-unitC["isExpensive"] = false
+unitA["id"]: Int = 122
 
 let unitB = Entity(type: "Unit")
 unitB["name"] = "UnitB"
-unitB["isExpensive"] = false
+unitC["isExpensive"] = false
+unitB["id"]: Int = 19
+
+let unitC = Entity(type: "Unit")
+unitC["name"] = "UnitC"
+unitC["isExpensive"] = true
+unitC["id"]: Int = 190
 
 graph.sync()
 
 let basicSearch = Search<Entity>(graph: graph).for(types: "Unit")
-print("Search brought \(basicSearch.sync().count) items.")
+basicSearch.sync()
+print("Search brought \(basicSearch.count) items.")
 
-let unitsSorted = Search<Entity>(graph: packageGraph).for(types: "Unit").sync().sorted(by: { (a: Entity, b: Entity) -> Bool in
-  return (a["name"] as! Int) < (b["name"] as! Int)
-})
-
-for unit in unitsSorted {
-  if let name = unit["name"] {
-    print("Unit name: \(name)")
-  }
-}
+//let unitsSorted = Search<Entity>(graph: graph).for(types: "Unit").sync().sorted(by: { (a: Entity, b: Entity) -> Bool in
+//  return (a["id"] as! Int) < (b["id"] as! Int)
+//})
+//
+//for unit in unitsSorted {
+//  if let name = unit["name"] {
+//    print("Unit ID: \(unit.id) name: \(name)")
+//  }
+//}
 
 //: [Next](@next)
