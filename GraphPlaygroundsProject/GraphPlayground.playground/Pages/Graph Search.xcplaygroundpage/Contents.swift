f082ae334e4a977410af35cd4080ff13e7b84566
Karsten Gresch <git-2015@by-doing.org>
2018-08-26 23:19:13 +0200

    Graph Playground Content

--- GraphPlaygroundsProject/GraphPlayground.playground/Pages/Graph Search.xcplaygroundpage/Contents.swift
+++ GraphPlaygroundsProject/GraphPlayground.playground/Pages/Graph Search.xcplaygroundpage/Contents.swift
@@ -1,9 +1,35 @@
-// Thanks to:
-// CosmicMind (Daniel Dahan) Graph:
-// Playground Setup: https://stackoverflow.com/a/45391022/4295602
+//: [Previous](@previous)
 
-import UIKit
+import Foundation
 import Graph
 
 let graph = Graph()
 
+let unitA = Entity(type: "Unit")
+unitA["name"] = "UnitA"
+unitA["isExpensive"] = true
+
+let unitC = Entity(type: "Unit")
+unitC["name"] = "UnitC"
+unitC["isExpensive"] = false
+
+let unitB = Entity(type: "Unit")
+unitB["name"] = "UnitB"
+unitB["isExpensive"] = false
+
+graph.sync()
+
+let basicSearch = Search<Entity>(graph: graph).for(types: "Unit")
+print("Search brought \(basicSearch.sync().count) items.")
+
+let unitsSorted = Search<Entity>(graph: packageGraph).for(types: "Unit").sync().sorted(by: { (a: Entity, b: Entity) -> Bool in
+  return (a["name"] as! Int) < (b["name"] as! Int)
+})
+
+for unit in unitsSorted {
+  if let name = unit["name"] {
+    print("Unit name: \(name)")
+  }
+}
+
+//: [Next](@next)
--- GraphPlaygroundsProject/GraphPlayground.playground/Pages/Graph Setup.xcplaygroundpage/Contents.swift
+++ GraphPlaygroundsProject/GraphPlayground.playground/Pages/Graph Setup.xcplaygroundpage/Contents.swift
@@ -3,6 +3,20 @@
 import Foundation
 import Graph
 
-var str = "Hello, playground"
+let graph = Graph()
+
+let unitA = Entity(type: "Unit")
+unitA["name"] = "UnitA"
+unitA["isExpensive"] = true
+
+let unitB = Entity(type: "Unit")
+unitB["name"] = "UnitB"
+unitB["isExpensive"] = false
+
+let unitC = Entity(type: "Unit")
+unitC["name"] = "UnitB"
+unitC["isExpensive"] = false
+
+graph.sync()
 
 //: [Next](@next)
